cbad0f712700abcc0ef825503c22900056144932
Илья <binocole77@gmail.com>
2025-08-19 01:52:01 +0300

    Add iOS project files

---
+++ CheckMenu/CheckMenuTests/CheckMenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMenuTests.swift
+//  CheckMenuTests
+//
+//  Created by Ilya Yakupov on 19.08.2025.
+//
+
+import Testing
+@testable import CheckMenu
+
+struct CheckMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
