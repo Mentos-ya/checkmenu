3f689be82ad8fa308033f067623ce2fdbd08cf90
Илья <binocole77@gmail.com>
2025-09-11 15:38:38 +0300

    Initial commit: CheckMenu iOS app with ML Kit integration

---
+++ CheckMenu/CheckMenu/CheckMenu/CameraView.swift
@@ -0,0 +1,48 @@
+//
+//  ImagePickerView.swift
+//  CheckMenu
+//
+//  Created by Ilya Yakupov on 19.08.2025.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    let sourceType: UIImagePickerController.SourceType
+    @Binding var capturedImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        picker.allowsEditing = false
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePickerView
+        
+        init(_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.capturedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
---
+++ CheckMenu/CheckMenu/CheckMenu/CheckMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMenuApp.swift
+//  CheckMenu
+//
+//  Created by Ilya Yakupov on 19.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CheckMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
